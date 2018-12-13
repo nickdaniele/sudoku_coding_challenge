26b723d3396adfde0273939324ee54b9bfe66a44
Nick Daniele <ngdaniele@gmail.com>
2018-12-13 02:39:17 -0800

    Finished.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,86 @@
-// Webpack entry point. Add code here to have it included and run in your Web app
+import Vue from 'vue';
+import axios from 'axios';
+import style from './style.css';
+import { sudokuVerifier } from './sudoku_verifier';
+
+document.addEventListener("DOMContentLoaded", function() {
+    new Vue({
+        el: document.getElementById('vue-hook'),
+        data: function() {
+            return {
+                problem: null,
+                solution: null,
+                validation: null,
+                error: null
+            }
+        },
+        created: function() {
+            this.loadNew();
+        },
+        computed: {
+            solutionGrid
+        },
+        methods: {
+            loadNew
+        }
+    });
+});
+
+
+// Computed
+function solutionGrid() {
+    let solution = this.solution;
+
+    if (solution) {
+        let solution = this.solution.slice();
+        let grid = [];
+    
+        while (solution.length > 0) {
+            const row = solution.splice(0, 9);
+        
+            grid.push(row);
+        }
+    
+        return grid;
+    }
+}
+
+// Methods
+function loadNew() {
+    axios.get('/sudoku')
+        .then((response) => {
+            let data = response.data;
+
+            let parsedForUser = determineUserInput(data);
+            let parsedForValid = determineValid(data, parsedForUser);
+            
+            this.problem = data.problem;
+            this.solution = parsedForValid;
+        })
+        .catch(() => {
+            this.error = 'There was an error fetching the sudoku puzzle.'
+        });
+}
+
+function determineUserInput(data) {
+    return data.solution.map((current, index) => {
+        let value = current === null? 'N' : current; 
+        let type = "user-input";
+        
+        if (data.problem[index] === null) {
+            type = "starter-input";
+        }
+
+        return { type, value };
+    });
+}
+
+function determineValid(data, parsed) {
+    let result = sudokuVerifier(data);
+
+    result.invalidIndexes.forEach((current) => {
+        parsed[current].type = 'invalid-input';
+    });
+
+    return parsed;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: [
     'babel-polyfill',
@@ -8,13 +10,31 @@ module.exports = {
     path: __dirname + '/build',
     publicPath: '/assets',
   },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
-      }
+      },
+      { 
+        test: /\.css$/, 
+        exclude: /\.useable\.css$/, 
+        loader: "style!css" },
+      { 
+        test: /\.useable\.css$/, 
+        loader: "style/useable!css" }
     ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Custom template',
+        template: __dirname + '/src/index.html'
+      })
+    ]
   },
 };
